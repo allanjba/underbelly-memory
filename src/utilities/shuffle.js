bfaff42fd8614856c7d90b3d2c83875ec6285559
allanjba <allan.ientz@gmail.com>
2022-03-23 11:14:33 -0600

    rem

--- src/utilities/shuffle.js
+++ src/utilities/shuffle.js
@@ -1,13 +1,18 @@
 const shuffle = () => {
     const assets = [
-        { image: '/assets/css.png' },
-        { image: '/assets/html5.png' },
-        { image: '/assets/jquery.png' },
-        { image: '/assets/js.png' },
-        { image: '/assets/next.png' },
-        { image: '/assets/node.png' },
-        { image: '/assets/react.png' },
-        { image: '/assets/ts.png' },
+        { image: '/assets/belly/allan.jpeg' },
+        { image: '/assets/belly/brad.jpeg' },
+        { image: '/assets/belly/devin.jpeg' },
+        { image: '/assets/belly/emily.jpeg' },
+        { image: '/assets/belly/caitlin2.jpeg' },
+        { image: '/assets/belly/mackayla.jpeg' },
+        { image: '/assets/belly/zach.jpeg' },
+        { image: '/assets/belly/martin.jpeg' },
+        { image: '/assets/belly/tony.jpeg' },
+        { image: '/assets/belly/taylor.jpeg' },
+        { image: '/assets/belly/michelle.jpeg' },
+        { image: '/assets/belly/jordan.jpeg' },
+
     ];
     return [...assets, ...assets]
         .sort(() => Math.random() - 0.5)
